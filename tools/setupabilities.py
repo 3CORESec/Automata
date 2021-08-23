b49f7113078fe8d2b93453ebe0609cfd41ad197a
Jonhnathan <jonhnathancesar@gmail.com>
2021-08-23 11:31:09 -0300

    Replace os.listdir with glob

--- tools/setupabilities.py
+++ tools/setupabilities.py
@@ -1,12 +1,13 @@
 import pysftp
 import setupcfg
 import os
+import glob
 
 srv = pysftp.Connection(host=setupcfg.CALDERA_HOST, username=setupcfg.SSH_USER, private_key=setupcfg.SSH_KEY)
 counter = 0
 with pysftp.cd(setupcfg.local_folder):
     srv.cwd(setupcfg.remote_folder)
-    for filename in os.listdir('.'):
+    for filename in glob.glob(setupcfg.local_folder + '/**/*.yml', recursive=True):
         srv.put(filename)
         counter += 1
 print("Uploaded {} abilities to Caldera".format(counter))
